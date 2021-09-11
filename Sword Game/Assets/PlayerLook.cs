522a87dd68948c56ed00057c2460e15c08330d3a
Chetangdev <84891627+Chetangdev@users.noreply.github.com>
2021-09-11 10:53:07 +0530

    A

---
+++ Sword Game/Assets/PlayerLook.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    public Transform player;
+
+    public float sensitivity = 10;
+
+    float x = 0;
+    float y = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+        //input
+        x += -Input.GetAxis("Mouse Y") * sensitivity;
+        y += Input.GetAxis("Mouse X") * sensitivity;
+
+        //clamping
+        x = Mathf.Clamp(x, -90, 90);
+
+        //rotation
+        transform.localRotation = Quaternion.Euler(x, 0, 0);
+        player.transform.localRotation = Quaternion.Euler(0, y, 0);
+
+
+        
+
+
+    }
+}
---
+++ Sword Game/Assets/SwordMechanics.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMechanics : MonoBehaviour
+{
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void NormalSwing()
+    {
+        
+    }
+
+    void Stab()
+    {
+
+    }
+
+    void UpAttack()
+    {
+
+    }
+
+    void JumpAttack()
+    {
+
+    }
+}
